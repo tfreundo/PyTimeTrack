f573cbbe74e5fd7120a2345a11fc02c124a41370
FRO3SI <fro3si@bosch.com>
2023-02-13 13:11:00 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PyTimeTrack
 Your minimalistic time tracking tool.
+
+## Configuration
+You can configure PyTimeTracker to your needs using the [config.toml](./config.toml).
+
+| Section | Param | Description |
+| ------- | ----- | ----------- |
+| development | devmode | Activates the development mode only necessary when developing features for this app |
---
+++ src/timetracker.py
@@ -0,0 +1,81 @@
+import tomllib
+from datetime import datetime
+import json
+import os
+
+
+class TimeTracker:
+    TIME_FORMAT = "%H:%M"
+
+    def track(self, report: dict) -> dict:
+        now = datetime.now()
+        today_str = now.strftime("%d.%m.%Y")
+        today_time_track = {"start": "", "end": "", "comment": ""}
+
+        if not (today_str in report.keys()):
+            # Create empty time track
+            print("Creating empty time track")
+            report[today_str] = today_time_track
+
+        if report[today_str] and report[today_str]["start"] == "":
+            start_time = now.strftime(self.TIME_FORMAT)
+            print(f"Tracking start time: {start_time}")
+            report[today_str]["start"] = start_time
+
+        elif report[today_str] and report[today_str]["start"] != "":
+            end_time = now.strftime(self.TIME_FORMAT)
+            print(f"Tracking end time: {end_time}")
+            report[today_str]["end"] = end_time
+
+        return report
+
+
+class MonthlyFileHandler:
+    EMPTY_FILESTRUCTURE = {}
+
+    def __init__(self, config: dict) -> None:
+        self.config = config
+        self.__create_monthly_file()
+
+    def current_report_filename(self) -> str:
+        today = datetime.today()
+        return (
+            f"reports/{today.month}_{today.year}.json"
+            if not self.config["development"]["devmode"]
+            else f"reports/DEV_{today.month}_{today.year}.json"
+        )
+
+    def read_current_report(self) -> dict:
+        with open(self.current_report_filename(), "r") as file:
+            return json.load(file)
+
+    def write_current_report(self, report: dict) -> None:
+        with open(self.current_report_filename(), "w") as file:
+            file.write(json.dumps(report, indent=2))
+
+    def __create_monthly_file(self):
+        """Creates a monthly report file if non exists"""
+        if os.path.exists(self.current_report_filename()):
+            print(
+                f'Monthly report file "{self.current_report_filename()}" already exists.'
+            )
+        else:
+            print(f'Creating report file "{self.current_report_filename()}".')
+            with open(self.current_report_filename(), "w") as file:
+                file.write(json.dumps(self.EMPTY_FILESTRUCTURE, indent=2))
+
+
+def main():
+    with open("config.toml", "rb") as config_file:
+        config = tomllib.load(config_file)
+
+    # Start app
+    fh = MonthlyFileHandler(config)
+    tt = TimeTracker()
+    report = fh.read_current_report()
+    report = tt.track(report)
+    fh.write_current_report(report)
+
+
+if __name__ == "__main__":
+    main()
